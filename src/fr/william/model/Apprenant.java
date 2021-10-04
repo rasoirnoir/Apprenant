1e3203aadd7dbaed569bf532c98235877b975e1b
William Noris <w.noris@protonmail.com>
2021-10-04 11:35:29 +0200

    initial commit

---
+++ src/fr/william/model/Apprenant.java
@@ -0,0 +1,45 @@
+package fr.william.model;
+
+public class Apprenant {
+	
+	private String prenom;
+	private String nom;
+	private int age;
+	
+	public Apprenant(String _prenom, String _nom, int _age) {
+		this.prenom = _prenom;
+		this.nom = _nom;
+		this.age = _age;
+	}
+	
+	public Apprenant() {
+		this(null, null, 0); //le mot clé "this" appel un autre constructeur
+	}
+	
+	public String getPrenom() {
+		return this.prenom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
